6d7c193579a6c1e8e790052f4528a79945fe12a5
Blaise <blaiselubuma@gmail.com>
2019-08-18 13:32:00 +0100

    Change the big text in the Home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import '../styles/Home.css';
+import React from "react";
+import "../styles/Home.css";
+import { Header } from "semantic-ui-react";
 
 const Home = () => {
   return (
     <div>
       <header className="home-header">
-        <h1>
+        {/* <h1>
           Graduation Project skeleton
-        </h1>
+        </h1> */}
+
+        <Header as="h1">Hello, Graduation Project</Header>
       </header>
     </div>
   );
