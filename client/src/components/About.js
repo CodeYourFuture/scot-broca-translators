b6576321ee9e13602d52a31634ee6b95718475ac
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-08-18 13:05:11 +0100

    update about pagw with more details

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        @scotlandclass3 These are the three projects you will build this year!
+        :tada: Translators/interpreters network: An app to create a user-based
+        network of volunteer interpreters/translators. Translators will be able
+        to create profiles with the language they speak. The History Project
+        (which is part of a real organisation) can upload chosen archival pieces
+        to the app and requests their translation from volunteer interpreters.
+        They can also upload invites and other texts for translation and
+        distribution. As a stretch goal, Users can also use the app as a
+        platform to upload texts or photos of texts in another language that
+        they do not understand, including what we commonly nickname “legalese”.
+      </p>
+    </div>
+  );
 };
 
 export default About;
