675585cb9610185af088fd9b511695e057fcc7fa
Adnanog <47014658+Adnanog@users.noreply.github.com>
2019-08-18 14:33:57 +0100

    Change the title and the name of the project to be relevant to our project (#4)

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import '../styles/Home.css';
+import React from "react";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <div>
       <header className="home-header">
-        <h1>
-          Graduation Project skeleton
-        </h1>
+        <h1>Broca Translation</h1>
       </header>
     </div>
   );
