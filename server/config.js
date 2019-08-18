66fc5a54a10b50e6b3676e541c386cd836fd4847
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-08-18 12:19:44 +0100

    change config user name to app_user

--- server/config.js
+++ server/config.js
@@ -1,14 +1,14 @@
 const config = {
-	development: {
-		user: "postgres",
-		host: "localhost",
-		database: "final_project",
-		password: "password",
-		port: 5432,
-	},
-	production: {
-		connectionString: process.env.DATABASE_URL,
-	},
+  development: {
+    user: "app_user",
+    host: "localhost",
+    database: "final_project",
+    password: "password",
+    port: 5432
+  },
+  production: {
+    connectionString: process.env.DATABASE_URL
+  }
 };
 
 const env = process.env.NODE_ENV || "development";
