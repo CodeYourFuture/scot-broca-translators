1307ff53048adf847fe74d3416cf8a239c7b06a1
Mohamed Lafiteh <47035574+mohamedlafiteh@users.noreply.github.com>
2019-08-18 16:17:13 +0100

    Restyle about us (#8)
    * Chnged the style of the text
    
    * Changed the index file removed about was double

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,17 +4,23 @@ const About = () => {
   return (
     <div>
       <h1>About us</h1>
+      <h1>Broca group</h1>
       <p>
-        @scotlandclass3 These are the three projects you will build this year!
-        :tada: Translators/interpreters network: An app to create a user-based
-        network of volunteer interpreters/translators. Translators will be able
-        to create profiles with the language they speak. The History Project
-        (which is part of a real organisation) can upload chosen archival pieces
-        to the app and requests their translation from volunteer interpreters.
+        These are the three projects you will build this year!
+        <br />
+        :tada: Translators/interpreters network: <br />
+        An app to create a user-based network of volunteer
+        interpreters/translators. Translators will be able to create profiles
+        with the language they speak.
+        <br /> The History Project (which is part of a real organisation) can
+        upload chosen archival pieces to the app and requests their translation
+        from volunteer interpreters.
+        <br />
         They can also upload invites and other texts for translation and
-        distribution. As a stretch goal, Users can also use the app as a
-        platform to upload texts or photos of texts in another language that
-        they do not understand, including what we commonly nickname “legalese”.
+        distribution. As a stretch goal,
+        <br /> Users can also use the app as a platform to upload texts or
+        photos of texts in another language that they do not understand, <br />
+        including what we commonly nickname “legalese”.
       </p>
     </div>
   );
