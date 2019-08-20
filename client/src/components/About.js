34fc0f0faf2729ba19335cb9b00d5da18c88a165
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-08-21 00:43:02 +0100

    Add some styling to the text and removed h1 tag

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,29 +1,37 @@
 import React from "react";
+import { Header, Icon, Grid } from "semantic-ui-react";
 
 const About = () => {
   return (
     <div>
-      <h1>About us</h1>
-      <h1>Broca group</h1>
-      <p>
-        These are the three projects you will build this year!
-        <br />
-        :tada: Translators/interpreters network: <br />
-        An app to create a user-based network of volunteer
-        interpreters/translators. Translators will be able to create profiles
-        with the language they speak.
+      <Header as="h2" icon textAlign="left">
+        <Icon name="users" />
+        <Header.Content style={textStyle}>
+          About Us: Broca Group Project
+        </Header.Content>
+      </Header>
+
+      <p style={textStyle}>
+        Translators/interpreters network: <br />
+        An app for a user-based network of volunteer interpreters/translators.
+        Translators able to create profiles with the language they speak.
         <br /> The History Project (which is part of a real organisation) can
         upload chosen archival pieces to the app and requests their translation
-        from volunteer interpreters.
         <br />
-        They can also upload invites and other texts for translation and
-        distribution. As a stretch goal,
+        from volunteer interpreters.They can also upload invites and other texts
+        for translation and distribution.
         <br /> Users can also use the app as a platform to upload texts or
-        photos of texts in another language that they do not understand, <br />
-        including what we commonly nickname “legalese”.
+        photos of texts in another language that they do not understand. <br />
       </p>
     </div>
   );
 };
 
 export default About;
+
+const textStyle = {
+  fontSize: "14px",
+  color: "black",
+  fontFamily: "Helvetica, Arial, sansSerif",
+  fontWeight: "bold"
+};
