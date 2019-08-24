5c8166b34fba47d88d6e260d01f81c8cf9b570db
Mariama <mariamasillah81@gmail.com>
2019-08-24 13:22:32 +0100

    Add validation to user registration page
    - Also changed confirmationpassword to camel case
    - Remove unnecessary console.log

--- client/src/components/UserRegisterForm.js
+++ client/src/components/UserRegisterForm.js
@@ -7,9 +7,10 @@ export class UserRegisterForm extends Component {
     super(props);
     this.state = {
       email: "",
-      username: "",
+      name: "",
       password: "",
-      confirmationpassword: ""
+      confirmationPassword: "",
+      userSignUp: false
     };
   }
 
@@ -20,54 +21,62 @@ export class UserRegisterForm extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-
-    const UserRequest = {
-      method: "POST",
-      body: JSON.stringify({
-        email: this.state.email,
-        username: this.state.username,
-        password: this.state.password,
-        confirmationpassword: this.state.confirmationpassword
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    };
-    fetch("http://localhost:4000/auth/register", UserRequest)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        this.setState({
-          email: "",
-          username: "",
-          password: "",
-          confirmationpassword: ""
-        });
-      });
+    const { password, confirmationPassword } = this.state;
+    if (password !== confirmationPassword) {
+      alert("passwords dont match");
+    } else {
+      const UserRequest = {
+        method: "POST",
+        body: JSON.stringify({
+          email: this.state.email,
+          name: this.state.name,
+          password: this.state.password,
+          confirmationPassword: this.state.confirmationPassword
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      };
+      fetch("http://localhost:4000/auth/register", UserRequest)
+        .then(res => res.json())
+        .then(this.resetForm);
+    }
   };
-
+  resetForm = () => {
+    this.setState({
+      name: "",
+      email: "",
+      password: "",
+      confirmationPassword: "",
+      userSignUp: "true"
+    });
+  };
   render() {
+    let userSignUp;
+    if (this.state.userSignUp) {
+      userSignUp = "User Logged in successfully";
+    } else {
+      userSignUp = null;
+    }
     return (
       <Grid centered column={16}>
         <Grid.Column width={6}>
           <Form onSubmit={this.handleSubmit}>
             <Form.Field>
+              <label>Name</label>
+              <input
+                onChange={this.handleChange}
+                value={this.state.name}
+                placeholder="Name"
+                name="name"
+                required
+              />
               <label>Email</label>
               <input
                 onChange={this.handleChange}
                 value={this.state.email}
                 placeholder="Email"
                 name="email"
-                required
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Username</label>
-              <input
-                value={this.state.username}
-                onChange={this.handleChange}
-                placeholder="Username"
-                name="username"
                 required
               />
             </Form.Field>
@@ -84,10 +93,10 @@ export class UserRegisterForm extends Component {
             <Form.Field>
               <label> Password Confirmation</label>
               <input
-                value={this.state.confirmationpassword}
+                value={this.state.confirmationPassword}
                 onChange={this.handleChange}
                 placeholder="Password confirmation"
-                name="confirmationpassword"
+                name="confirmationPassword"
                 required
               />
             </Form.Field>
@@ -99,6 +108,7 @@ export class UserRegisterForm extends Component {
             <Button secondary>
               <Link to="/">Cancel</Link>{" "}
             </Button>
+            <h1>{userSignUp}</h1>
           </Form>
         </Grid.Column>
       </Grid>
