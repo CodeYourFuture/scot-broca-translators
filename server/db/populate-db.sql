ed09dcfb75303b7a49ddd52916d99706df45186d
Adnan <adnanmh82@gmail.com>
2019-08-24 23:56:51 +0100

    Add format types as enum and change the date format

--- server/db/populate-db.sql
+++ server/db/populate-db.sql
@@ -1,7 +1,8 @@
 -- Users seed data
--- Note: Do NOT add passwords you use in real life. Passwords are now saved as clear text. This is very bad from security point of view. We will have a task to encrypt the passwords
+
 INSERT INTO users (id, name,email, password, role) values ('db5b2e53-acd2-4af0-bd91-8554d9dc09ef', 'Daniel','admin@cyf.org', 'admin_password','Interpreter');
 INSERT INTO users (id, name,email, password, role) values ('a2deeae9-1d8c-44ef-a187-a85dfa56fee8', 'Nada','user@cyf.org', 'user_password', 'User');
+
 INSERT INTO languages (code, name) VALUES ('zh-cn','Mandarin Chinese');
 INSERT INTO languages (code, name) VALUES ('ar','Arabic');
 INSERT INTO languages (code, name) VALUES ('es','Spanish');
@@ -12,5 +13,5 @@ INSERT INTO languages (code, name) VALUES ('be','Bengali');
 INSERT INTO languages (code, name) VALUES ('fr','French');
 
 
-INSERT INTO documents (from_language_code, to_language_code, status, submission_date, due_date, owner_id, format) values ('ar','en', 'Waiting', '20-01-2019','20-02-2019','db5b2e53-acd2-4af0-bd91-8554d9dc09ef','string');
-INSERT INTO documents (from_language_code, to_language_code, status, submission_date, due_date, owner_id, format) values ('zh-cn','en', 'Processing', '20-07-2019','2-08-2019', 'a2deeae9-1d8c-44ef-a187-a85dfa56fee8', 'string');
+INSERT INTO documents (from_language_code, to_language_code, status, submission_date, due_date, owner_id, format) values ('ar','en', 'Waiting', '2019-01-20','2019-02-20','db5b2e53-acd2-4af0-bd91-8554d9dc09ef','Text');
+INSERT INTO documents (from_language_code, to_language_code, status, submission_date, due_date, owner_id, format) values ('zh-cn','en', 'Processing', '2019-07-20','2019-08-02', 'a2deeae9-1d8c-44ef-a187-a85dfa56fee8', 'Text');
