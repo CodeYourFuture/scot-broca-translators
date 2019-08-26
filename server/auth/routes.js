7949c84f32736e60592db8b13593f7aee212a8b4
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-08-26 13:19:49 +0100

    Add validation and role to interpreter regi

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -7,48 +7,64 @@ const db = require("../services/database/users");
  * Users Login
  */
 router.post("/login", async (req, res, next) => {
-	const { email, password } = req.body;
-	console.log(`Login attempt ${email}`);
-	try {
-		const user = await db.getUserByEmail(email);
+  const { email, password } = req.body;
+  console.log(`Login attempt ${email}`);
+  try {
+    const user = await db.getUserByEmail(email);
 
-		if (!email || !password || !user || password !== user.password) {
-			return res.sendStatus(403);
-		}
+    if (!email || !password || !user || password !== user.password) {
+      return res.sendStatus(403);
+    }
 
-		const secret = process.env.JWT_SECRET || "your_jwt_secret";
-		const token = jwt.sign({ userId: user.id }, secret);
-		delete user.password;
-		delete user.salt;
-		return res.send({ token, user });
-	} catch (e) {
-		console.error(e);
-		next(e);
-	}
+    const secret = process.env.JWT_SECRET || "your_jwt_secret";
+    const token = jwt.sign({ userId: user.id }, secret);
+    delete user.password;
+    delete user.salt;
+    return res.send({ token, user });
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
 });
 
 /**
  * Users Registration
  */
 router.post("/register", async (req, res, next) => {
-	const { email, password } = req.body;
+  const { email, password, username, role } = req.body;
 
-	const user = {
-		email,
-		password
-	};
-
-	db.createUser(user)
-		.then(() => {
-			res.send({
-				success: true,
-				message: "Account created",
-			});
-		})
-		.catch((err) => {
-			console.log(err);
-			next(err);
-		});
+  const user = {
+    email,
+    password,
+    username,
+    role
+  };
+  console.log("req body" + user);
+  console.log("req body" + user.email);
+  if (
+    user.email != null &&
+    user.email.length > 1 &&
+    user.password.length > 1 &&
+    user.username.length > 1 &&
+    user.role.length > 1
+  ) {
+    db.createUser(user)
+      .then(() => {
+        res.send({
+          success: true,
+          message: "Account created"
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        next(err);
+      });
+  } else {
+    res.send({
+      success: false,
+      message: "Empty Validation"
+    });
+  }
 });
 
 module.exports = router;
--- server/services/database/users.js
+++ server/services/database/users.js
@@ -8,60 +8,60 @@ const pool = new Pool(config);
  * security risk to expose all users
  */
 const getAllUsers = () => {
-	return new Promise((resolve, reject) => {
-		pool.query("SELECT id, email FROM users", (error, result) => {
-			if (error) {
-				reject(error);
-			} else {
-				resolve(result.rows);
-			}
-		});
-	});
+  return new Promise((resolve, reject) => {
+    pool.query("SELECT id, email FROM users", (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result.rows);
+      }
+    });
+  });
 };
 
-const getUserByEmail = (email) => {
-	return new Promise((resolve) => {
-		pool.query(
-			"SELECT * FROM users where email = $1",
-			[email],
-			(error, result) => {
-				resolve(result.rows[0]);
-			}
-		);
-	});
+const getUserByEmail = email => {
+  return new Promise(resolve => {
+    pool.query(
+      "SELECT * FROM users where email = $1",
+      [email],
+      (error, result) => {
+        resolve(result.rows[0]);
+      }
+    );
+  });
 };
 
-const createUser = ({ email, password }) => {
-	return new Promise((resolve, reject) => {
-		pool.query(
-			"INSERT INTO users (email, password) values ($1, $2)",
-			[email, password],
-			(error, result) => {
-				if (error) {
-					reject(error);
-				}
-				console.log(result);
-				resolve(result.rows);
-			}
-		);
-	});
+const createUser = ({ email, password, name, role }) => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "INSERT INTO users (email, password,name,role) values ($1, $2,$3,$4)",
+      [email, password, name, role],
+      (error, result) => {
+        if (error) {
+          reject(error);
+        }
+        console.log(result);
+        resolve(result.rows);
+      }
+    );
+  });
 };
 
-const getUserById = (id) => {
-	return new Promise((resolve, reject) => {
-		pool.query("SELECT * FROM users where id = $1", [id], (error, result) => {
-			if (error) {
-				console.error(error);
-				return reject(error);
-			}
-			resolve(result.rows[0]);
-		});
-	});
+const getUserById = id => {
+  return new Promise((resolve, reject) => {
+    pool.query("SELECT * FROM users where id = $1", [id], (error, result) => {
+      if (error) {
+        console.error(error);
+        return reject(error);
+      }
+      resolve(result.rows[0]);
+    });
+  });
 };
 
 module.exports = {
-	getUserByEmail,
-	createUser,
-	getUserById,
-	getAllUsers,
+  getUserByEmail,
+  createUser,
+  getUserById,
+  getAllUsers
 };
