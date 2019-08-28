447200e34e69006d633b310eebc76c326e34b709
Adnan <adnanmh82@gmail.com>
2019-08-28 19:34:34 +0100

    Add semantic ui to the buttons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -17,22 +17,24 @@ const Home = () => {
         className="homePageImg"
       />
       <div className="information">
-        <p className="parag-one">
-          An app to create a user-based network of volunteer
-          interpreters/translators. Translators will be able to create profiles
-          with the language they speak.The History Project (which is part of a
-          real organisation) can upload chosen archival pieces to the app and
-          requests translation.
-        </p>
-        <p className="parag-two">
-          They can also upload invites and other texts for translation and
-          distribution. As a stretch goal, Users can also use the app as a
-          platform to upload texts or photos of texts in another language that
-          they do not understand, including what we commonly nickname
-          “legalese”.
-        </p>
-        <div className="homePage-buttons">
+        <div className="parag-one">
+          <p>
+            An app to create a user-based network of volunteer
+            interpreters/translators. Translators will be able to create
+            profiles with the language they speak.The History Project (which is
+            part of a real organisation) can upload chosen archival pieces to
+            the app and requests translation.
+          </p>
           <Button primary content="Sign Up" />
+        </div>
+        <div className="parag-two">
+          <p>
+            They can also upload invites and other texts for translation and
+            distribution. As a stretch goal, Users can also use the app as a
+            platform to upload texts or photos of texts in another language that
+            they do not understand, including what we commonly nickname
+            “legalese”.
+          </p>
           <Button primary content="Become an Interprter" />
         </div>
       </div>
