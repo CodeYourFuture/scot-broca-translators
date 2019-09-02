2ddf765d28001307c65b2134d4a14279b8895dc6
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-09-02 21:47:22 +0100

    Remove the space in the langauge file

--- client/src/components/LanguageOptions.js
+++ client/src/components/LanguageOptions.js
@@ -1,9 +1,8 @@
 const languageOptions = [
-  { text: " Mandarin Chinese", value: "zh-cn" },
+  { text: "Mandarin Chinese", value: "zh-cn" },
   { text: "Arabic", value: "ar" },
   { text: "Spanish", value: "es" },
   { text: "Malaysian", value: "ml" },
-
   { text: "English", value: "en" },
   { text: "Russian", value: "ru" },
   { text: "Bengali", value: "be" },
