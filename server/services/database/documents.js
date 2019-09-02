cd81e3d55b344d92bf7acae85517c0d462ecafa8
Vicky <kosenchiha@gmail.com>
2019-09-03 00:48:17 +0100

    Display interpreters' name in the dashboard for processing documents

--- server/services/database/documents.js
+++ server/services/database/documents.js
@@ -18,8 +18,30 @@ const query =
       on u.id = d.owner_id";
 
 const getAllDocuments = () => {
-  const sqlQuery = query + ";";
-
+  const sqlQuery = `select\
+  d.id, d.format, d.name,\
+    d.status, d.submission_date, d.due_date,\
+    lf.name as from_language_name,\
+    lt.name as to_language_name,\
+    u.name as owner_name,\
+    trnsl.start_date, trnsl.translator_name\
+  from documents as d\
+  inner join languages as lf\
+  on  lf.code = d.from_language_code\
+  inner join languages as lt\
+  on lt.code = d.to_language_code\
+  inner join users as u\
+  on u.id = d.owner_id\
+  left join(\
+    select\
+           t.document_id,\
+    t.start_date,\
+    tu.name as translator_name\
+    from translations as t\
+    inner join users as tu\
+        on tu.id = t.user_id\
+  ) trnsl on trnsl.document_id = d.id\
+    ;`;
   return pool
     .query(sqlQuery)
     .then(result => result.rows)
