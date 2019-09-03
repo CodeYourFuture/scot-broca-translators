00141ee9f257fdb9fb856eb1c94521aec24a04f3
Mariama <mariamasillah81@gmail.com>
2019-09-03 11:44:48 +0100

    Add the createDocument function back to documents file. Add language back to the LanguageOptions file.

--- server/services/database/documents.js
+++ server/services/database/documents.js
@@ -2,6 +2,39 @@ const { Pool } = require("pg");
 const config = require("../../config");
 const pool = new Pool(config);
 
+const createDocument = ({
+  from_language_code,
+  to_language_code,
+  submission_date,
+  due_date,
+  owner_id,
+  name,
+  format,
+  content
+}) => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "INSERT INTO documents (from_language_code,to_language_code,submission_date,due_date,owner_id,name,format,content) values ($1, $2,$3,$4,$5,$6,$7,$8)",
+      [
+        from_language_code,
+        to_language_code,
+        submission_date,
+        due_date,
+        owner_id,
+        name,
+        format,
+        content
+      ],
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result.rows);
+        }
+      }
+    );
+  });
+};
 const query =
   "select\
           d.id, d.format, d.name,\
