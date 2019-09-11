bd094929f46e55d9c05394eba454503130c31d47
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-09-11 19:46:16 +0100

    Move the translations functions to translation file

--- server/api/documents.js
+++ server/api/documents.js
@@ -4,8 +4,6 @@ const docsDb = require("../services/database/documents");
 const { INTERPRETER } = require("../auth/roles");
 const router = express.Router();
 
-
-
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
@@ -75,47 +73,5 @@ router.get(
       });
   }
 );
-
-router.put(
-  `/:id/translations`,
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    const translationId = req.params.id;
-    const content = req.body.content;
-
-    if (content != null && content.length > 0) {
-      docsDb
-        .updateTranslation(content, translationId)
-        .then(data => {
-          console.log("this is what i am getting fom update" + data);
-          if (data != null && data.length > 0) {
-            getDocumentIdByTranslationId(translationId).then(data => {
-              const documentId = data;
-              updateDocumentStatusById(documentId, "Completed");
-            });
-            res.send(data);
-          } else {
-            // send error
-            res.status(404).send("the document is not found");
-          }
-        })
-        .catch(err => {
-          res.send(500);
-        });
-    } else {
-      res.status(400).send("pleaase inter content to update");
-    }
-  }
-);
-
-const updateDocumentStatusById = documentId => {
-  docsDb.updateDocumentStatusById(documentId, "Completed");
-};
-
-const getDocumentIdByTranslationId = translationId => {
-  return docsDb.getDocumentIdByTranslationId(translationId).then(data => {
-    return data[0].document_id;
-  });
-};
 
 module.exports = router;
