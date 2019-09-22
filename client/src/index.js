a5facdfab5f359bf40b1bdb6166463bda5c8cc56
Adnan <adnanmh82@gmail.com>
2019-09-22 16:11:07 +0100

    Update the navigation bar

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import "semantic-ui-css/semantic.min.css";
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Home from "./components/Home";
@@ -10,109 +10,146 @@ import InterpreterSignUpForm from "./components/InterpreterSignUpForm";
 import AddDocumentForm from "./components/AddDocumentForm";
 import Dashboard from "./components/Dashboard";
 import AddTranslationForm from "./components/AddTranslationForm";
-import { Menu } from "semantic-ui-react";
+import { Menu, Image, Container } from "semantic-ui-react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import { isLoggedIn } from "./components/helpers/isLoggedIn";
 import { isUser } from "./components/helpers/isUser";
 import ViewDocument from "./components/ViewDocument";
 
-const logout = () => {
-  sessionStorage.clear();
-  window.location.href = "/";
-};
+class Routes extends Component {
+  state = {};
 
-const Routes = () => {
-  return (
-    <Router>
-      <div>
-        <Menu>
-          <Link className="nav-link" to="/">
-            <Menu.Item name="editorials" active={true}>
-              Home
-            </Menu.Item>
-          </Link>
-          {isLoggedIn() ? (
-            <Link className="nav-link" to="/Dashboard">
-              <Menu.Item name="editorials" active={true}>
-                Dashboard
-              </Menu.Item>
-            </Link>
-          ) : null}
-          <Link className="nav-link" to="/about">
-            <Menu.Item name="editorials" active={true}>
-              About
-            </Menu.Item>
-          </Link>
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
-          <Link className="nav-link" to="/status">
-            <Menu.Item name="editorials" active={true}>
-              Status
-            </Menu.Item>
-          </Link>
+  logout = () => {
+    sessionStorage.clear();
+    window.location.href = "/";
+  };
 
-          {isLoggedIn() ? (
-            <Menu.Menu position="right">
-              <Link className="nav-link" to="/add-document/">
-                <Menu.Item name="editorials" active={true}>
-                  Add document
-                </Menu.Item>
-              </Link>
-
-              <Menu.Item active={true} onClick={logout}>
-                Logout
-              </Menu.Item>
-            </Menu.Menu>
-          ) : (
-            <Menu.Menu>
-              <Link className="nav-link" to="/sign-up-user">
-                <Menu.Item name="editorials" active={true}>
-                  User Sign Up
-                </Menu.Item>
-              </Link>
-
-              <Link className="nav-link" to="/login">
-                <Menu.Item name="editorials" active={true}>
-                  Login
-                </Menu.Item>
-              </Link>
-
-              <Link className="nav-link" to="/sign-up-interpreter">
-                <Menu.Item name="editorials" active={true}>
-                  Become Interpreter
-                </Menu.Item>
-              </Link>
-            </Menu.Menu>
-          )}
-        </Menu>
+  render() {
+    const { activeItem } = this.state;
+    return (
+      <Router>
         <div>
-          <Route path="/" exact component={Home} />
-          <Route path="/about/" component={About} />
-          <Route path="/status/" component={Status} />
-          <Route path="/sign-up-user/" component={UserSignUpForm} />
-          <Route path="/login/" component={Login} />
-          <Route
-            path="/sign-up-interpreter/"
-            component={InterpreterSignUpForm}
-          />
-          {isLoggedIn() ? (
-            <div>
-              <Route path="/add-document/" component={AddDocumentForm} />
-              <Route path="/dashboard" component={Dashboard} />
-              {isUser() ? null : (
-                <Route
-                  path="/add-document-translation/:documentId"
-                  component={AddTranslationForm}
+          <Menu borderless inverted color="grey">
+            <Container>
+              <Menu.Item
+                as={Link}
+                to="/"
+                header
+                name="broca translation"
+                active={activeItem === "broca translation"}
+                onClick={this.handleItemClick}
+              >
+                <Image
+                  src="https://image.flaticon.com/icons/svg/2014/2014350.svg"
+                  style={{ marginRight: "1.5em" }}
+                  size="mini"
                 />
+                Broca Translation
+              </Menu.Item>
+              <Menu.Item
+                name="home"
+                as={Link}
+                to="/"
+                header
+                active={activeItem === "home"}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                name="about"
+                as={Link}
+                to="/about"
+                header
+                active={activeItem === "about"}
+                onClick={this.handleItemClick}
+              />
+              {isLoggedIn() ? (
+                <Menu.Menu position="left">
+                  <Menu.Item
+                    name="dashboard"
+                    as={Link}
+                    to="/Dashboard"
+                    header
+                    active={activeItem === "dashboard"}
+                    onClick={this.handleItemClick}
+                  />
+                  {isUser() ? (
+                    <Menu.Item
+                      name="add document"
+                      as={Link}
+                      to="/add-document"
+                      header
+                      active={activeItem === "add document"}
+                      onClick={this.handleItemClick}
+                    />
+                  ) : null}
+                  <Menu.Item
+                    onClick={this.logout}
+                    as={Link}
+                    header
+                    name="logout"
+                  />
+                </Menu.Menu>
+              ) : (
+                <Menu.Menu position="left">
+                  <Menu.Item
+                    name="login"
+                    as={Link}
+                    to="/login"
+                    header
+                    active={activeItem === "login"}
+                    onClick={this.handleItemClick}
+                  />
+                  <Menu.Item
+                    name="user registration"
+                    as={Link}
+                    to="/sign-up-user"
+                    header
+                    active={activeItem === "user registration"}
+                    onClick={this.handleItemClick}
+                  />
+                  <Menu.Item
+                    name="interpreter registration"
+                    as={Link}
+                    to="/sign-up-interpreter"
+                    header
+                    active={activeItem === "interpreter registration"}
+                    onClick={this.handleItemClick}
+                  />
+                </Menu.Menu>
               )}
-
-              <Route path="/document/:id" component={ViewDocument} />
-            </div>
-          ) : null}
+            </Container>
+          </Menu>
+          <div>
+            <Route path="/" exact component={Home} />
+            <Route path="/about/" component={About} />
+            <Route path="/status/" component={Status} />
+            <Route path="/sign-up-user/" component={UserSignUpForm} />
+            <Route path="/login/" component={Login} />
+            <Route
+              path="/sign-up-interpreter/"
+              component={InterpreterSignUpForm}
+            />
+            {isLoggedIn() ? (
+              <div>
+                <Route path="/add-document/" component={AddDocumentForm} />
+                <Route path="/dashboard" component={Dashboard} />
+                {isUser() ? null : (
+                  <Route
+                    path="/add-document-translation/:documentId"
+                    component={AddTranslationForm}
+                  />
+                )}
+                <Route path="/document/:id" component={ViewDocument} />
+              </div>
+            ) : null}
+          </div>
         </div>
-      </div>
-    </Router>
-  );
-};
+      </Router>
+    );
+  }
+}
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
