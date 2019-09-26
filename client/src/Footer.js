5fff12bfbb6cae2c0ce3cabad7b2fa5d2ba3ec51
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-09-26 23:29:30 +0100

    Fix the footer links style

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -30,16 +30,15 @@ export const Footer = () => (
             <p>
               Broca Translation is an open source project hosted on <Icon name="github" /> <a title="github"
                 className="footerStyle"
-                href="https://github.com/CodeYourFuture/scot-broca-translators"> Github.
-              </a>
+                href="https://github.com/CodeYourFuture/scot-broca-translators"> Github.</a>
               <br />
               Thanks to <a
                 title="smashicons"
                 className="footerStyle"
-                href="https://www.flaticon.com/authors/smashicons>Smashicons"> Smashicons </a> for the logo and to<a
+                href="https://www.flaticon.com/authors/smashicons>Smashicons"> Smashicons </a>for the logo and to<a
                   title="unsplash"
                   className="footerStyle"
-                  href="https://unsplash.com/"> Unsplash </a> for the free images.</p>
+                  href="https://unsplash.com/"> Unsplash </a>for the free images.</p>
           </Grid.Column>
         </Grid.Row>
       </Grid>
