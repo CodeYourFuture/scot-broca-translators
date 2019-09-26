5891121a2027d7499f9df48c19bc0dd1ef97ac62
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-09-26 22:05:12 +0100

    Fix the footer text

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -28,37 +28,18 @@ export const Footer = () => (
               Broca Translation
             </Header>
             <p>
-              Broca Translation is an open source project hosted on{" "}
-              <Icon name="github" />{" "}
-              <a
-                title="github"
+              Broca Translation is an open source project hosted on <Icon name="github" /> <a title="github"
                 className="footerStyle"
-                href="https://github.com/CodeYourFuture/scot-broca-translators"
-              >
-                {" "}
-                Github.
+                href="https://github.com/CodeYourFuture/scot-broca-translators"> Github.
               </a>
               <br />
-              Thanks to{" "}
-              <a
+              Thanks to <a
                 title="smashicons"
                 className="footerStyle"
-                href="https://www.flaticon.com/authors/smashicons>Smashicons"
-              >
-                {" "}
-                Smashicons{" "}
-              </a>{" "}
-              for the logo and to
-              <a
-                title="unsplash"
-                className="footerStyle"
-                href="https://unsplash.com/"
-              >
-                {" "}
-                Unsplash{" "}
-              </a>{" "}
-              for the free images.
-            </p>
+                href="https://www.flaticon.com/authors/smashicons>Smashicons"> Smashicons </a> for the logo and to<a
+                  title="unsplash"
+                  className="footerStyle"
+                  href="https://unsplash.com/"> Unsplash </a> for the free images.</p>
           </Grid.Column>
         </Grid.Row>
       </Grid>
