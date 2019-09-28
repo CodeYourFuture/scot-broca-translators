8cbeb7f6fd7f4a91594b8c08885320aaa93ce69a
Blaise <blaiselubuma@gmail.com>
2019-09-28 11:37:10 +0100

    Add card with image to the About page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,28 +1,35 @@
 import React from "react";
-import { Header, Icon } from "semantic-ui-react";
+import {
+  Header,
+  Grid,
+  Segment,
+  Icon,
+  Card,
+  Image,
+  CardDescription
+} from "semantic-ui-react";
 
 const About = () => {
   return (
     <div>
-      <Header as="h2" icon textAlign="left">
-        <Icon name="users" />
+      <Header as="h2" icon textAlign="centre">
         <Header.Content style={textStyle}>
-          About Us: Broca Group Project
+          About Broca Translation
         </Header.Content>
       </Header>
 
-      <p style={textStyle}>
-        Translators/interpreters network: <br />
-        An app for a user-based network of volunteer interpreters/translators.
-        Translators able to create profiles with the language they speak.
-        <br /> The History Project (which is part of a real organisation) can
-        upload chosen archival pieces to the app and requests their translation
-        <br />
-        from volunteer interpreters.They can also upload invites and other texts
-        for translation and distribution.
-        <br /> Users can also use the app as a platform to upload texts or
-        photos of texts in another language that they do not understand. <br />
-      </p>
+      <Card
+        centered
+        image="resources/about.jpg"
+        description="An app for a user-based network of volunteer interpreters/translators. Translators
+          able to create profiles with the language they speak. The History
+          Project (which is part of a real organisation) can upload chosen
+          archival pieces to the app and requests their translation from
+          volunteer interpreters.They can also upload invites and other texts
+          for translation and distribution. Users can also use the app as a
+          platform to upload texts or photos of texts in another language that
+          they do not understand."
+      />
     </div>
   );
 };
@@ -30,7 +37,7 @@ const About = () => {
 export default About;
 
 const textStyle = {
-  fontSize: "14px",
+  fontSize: "20px",
   color: "black",
   fontFamily: "Helvetica, Arial, sansSerif",
   fontWeight: "bold"
