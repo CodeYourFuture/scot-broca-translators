f82aea66046f51a539a5897d436943e55bfbc7b1
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-10-01 13:42:30 +0100

    Display greeting based on the time of the day

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -126,10 +126,20 @@ class Dashboard extends Component {
       { header: "To Language", sortKey: "to_language_name" },
       { header: "Status", sortKey: "status" }
     ];
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
     return (
       <Container>
         <Header style={{ marginTop: "25px" }} as="h2">
-          Hello {userName}!
+          Good {timeOfDay} {userName}!
         </Header>
         {userRole === "User" ? (
           <Button
